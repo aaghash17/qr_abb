91dc8efda88721fcd721e4f4a2ccf435ca178cca
Aaghash R <aaghash.r@in.abb.com>
2024-03-19 13:49:08 +0530

    next

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useSearchParams } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [searchParams, setSearchParams] = useSearchParams();
+
+  const [searchParams] = useSearchParams();
+  const ContName = searchParams.get("ContName");
+  const ContID = searchParams.get("ContID");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <p>Controller Information</p>
+      {ContName && <p> Controller Name: {ContName}</p>}
+      {ContID && <p>Controller ID: {ContID}</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
