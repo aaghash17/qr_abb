0de08d8dac2e88c44c704de44bdfc85c7910121f
Aaghash R <aaghash.r@in.abb.com>
2024-03-19 14:24:08 +0530

    before final

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useSearchParams } from "react-router-dom";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
+import ABBlogo from "./assets/abb-logo.svg";
 
 function App() {
   //const [searchParams, setSearchParams] = useSearchParams();
@@ -10,10 +12,22 @@ function App() {
   const ContID = searchParams.get("ContID");
 
   return (
-    <div>
-      <p>Controller Information</p>
-      {ContName && <p> Controller Name: {ContName}</p>}
-      {ContID && <p>Controller ID: {ContID}</p>}
+    <div className="container-fluid">
+      <div className="row justify-content-center">
+        <div className="col-lg-6">
+          <div className="row align-items-center">
+            <div className="col-4">
+              <img src={ABBlogo} alt="Logo" className="img-fluid" />
+            </div>
+            <div className="col-8">
+              <h3>Controller Insight</h3>
+              <p></p>
+              {ContName && <p>Controller Name: {ContName}</p>}
+              {ContID && <p>Controller ID: {ContID}</p>}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
